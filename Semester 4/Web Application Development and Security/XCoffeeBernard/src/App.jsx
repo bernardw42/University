ebc3c800da47fb7d537f5b7b80874159c16bbde7
Bernard Wijaya <97165435+bernardw42@users.noreply.github.com>
2022-06-28 18:02:29 +0700

    Add files via upload

---
+++ Semester 4/Web Application Development and Security/XCoffeeBernard/src/App.jsx
@@ -0,0 +1,9 @@
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
---
+++ Semester 4/Web Application Development and Security/XCoffeeBernard/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.coffeecommune.com.au/uploads/2021/10/Coffee-Com-Cafe-33-copy-1536x1469-op-1024x979.jpg",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON TASTE! GET FLAT 30% OFF FOR NEW COMERS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "http://assets.stickpng.com/images/580b57fbd9996e24bc43c0b7.png",
+      title: "SUMMER COLLECTION",
+      desc: "DON'T COMPROMISE ON TASTE! GET FLAT 30% OFF FOR NEW COMERS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://www.pngpix.com/wp-content/uploads/2016/10/PNGPIX-COM-Cake-PNG-Transparent-Image-3.png",
+      title: "SWEET LOVE",
+      desc: "DON'T COMPROMISE ON TASTE! GET FLAT 30% OFF FOR NEW COMERS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://cdn.shopify.com/s/files/1/0107/2919/3531/files/ice_cream_with_sprinkles_600x.jpg?v=1621447653",
+      title: "Sundaes",
+    },
+    {
+      id: 2,
+      img: "https://www.centralparkjakarta.com/wp-content/uploads/2021/02/arabica-1.jpeg",
+      title: "Coffees",
+    },
+    {
+      id: 3,
+      img: "https://blog.tokowahab.com/wp-content/uploads/2021/11/Resep-Java-Chip-Frappuccino-Cocok-untuk-Ide-Jualan-Minuman-Kekinian.-Simak-di-Blog.Tokowahab.com_.png",
+      title: "Frapucino",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://cdn.bitrix24.id/b14044525/landing/de6/de6e5e30d25c19b6cba2653cfa61df6d/Coffee_Toffee_-_Gallery_04b_-_Take_Away_1x.jpg",
+    },
+    {
+      id:2,
+      img:"https://cdn.bitrix24.id/b14044525/landing/3a6/3a69ebfa7ef860848b21a97f39b62745/Coffee_Toffee_-_Gallery_03b_-_Happy_Customer_1x.jpg",
+    },
+    {
+      id:3,
+      img:"https://cdn.bitrix24.id/b14044525/landing/1b0/1b05c5c9c6bda019158adf36dffa01af/Coffee_Toffee_-_Gallery_05b_-_Barista_Brewing_1x.jpg",
+    },
+    {
+      id:4,
+      img:"https://cdn.bitrix24.id/b14044525/landing/713/71374217a1e039e6b8ca376aa7f090e4/Coffee_Toffee_-_Gallery_10b_-_Iced_Coffee_for_Everyone_1x.jpg",
+    },
+    {
+      id:5,
+      img:"https://cdn.bitrix24.id/b14044525/landing/060/060ed8272fc71a29cf47dacb3d6a0d07/Coffee_Toffee_-_Gallery_01b_-_Menyerahkan_gelas_take_away_1x.jpg",
+    },
+    {
+      id:6,
+      img:"https://cdn.bitrix24.id/b14044525/landing/067/067feb0376120499b44a3fb85d2100fe/Coffee_Toffee_-_Gallery_07b_-_Pride_in_every_cup_1x.jpg",
+    },
+    {
+      id:7,
+      img:"https://img.jakpost.net/c/2021/01/24/2021_01_24_110085_1611480659._large.jpg",
+    },
+    {
+      id:8,
+      img:"https://nowjakarta.co.id/uploads/ckeditor/pictures/2851/content_nathan-dumlao-6VhPY27jdps-unsplash.jpg",
+    },
+  ]
